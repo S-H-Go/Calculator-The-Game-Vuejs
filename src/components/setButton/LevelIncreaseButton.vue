99ff74a7e5d96e2f1f65879e91800e90f0df7c07
S-H-G <wangshijiehh@outlook.com>
2022-08-26 22:19:48 +0800

    🐞 fix(关卡增加、减少按钮): 修复触摸长按无法持续增、减关卡

--- src/components/setButton/LevelIncreaseButton.vue
+++ src/components/setButton/LevelIncreaseButton.vue
@@ -1,5 +1,6 @@
 <template>
-    <button @mousedown="continuousIncrease()" @mouseup="stopIncrease()" @mouseleave="stopIncrease()"
+    <button @touchstart="continuousIncrease('touchstart')" @touchcancel="stopIncrease()" @touchend="stopIncrease"
+        @mousedown="continuousIncrease('mousedown')" @mouseup="stopIncrease()" @mouseleave="stopIncrease()"
         class=" button color-button-6c96b6">+</button>
 </template>
 
@@ -7,13 +8,13 @@
 import { useStore } from '../../store'
 const store = useStore()
 let increase = setInterval(() => { });
-function continuousIncrease() {
+function continuousIncrease(event: string) {
     let isClick = true;
     increase = setInterval(() => {
         isClick = false;
         store.levelIncrease();
     }, 240);
-    isClick ? store.levelIncrease() : "";
+    isClick && event == 'mousedown' ? store.levelIncrease() : "";
 }
 function stopIncrease() {
     clearInterval(increase);
--- src/components/setButton/LevelReductionButton.vue
+++ src/components/setButton/LevelReductionButton.vue
@@ -1,5 +1,6 @@
 <template>
-    <button @mousedown="continuousReduction()" @mouseup="stopReduction()" @mouseleave="stopReduction()"
+    <button @touchstart="continuousReduction('touchstart')" @touchcancel="stopReduction()" @touchend="stopReduction()"
+        @mousedown="continuousReduction('mousedown')" @mouseup="stopReduction()" @mouseleave="stopReduction()"
         class="button color-button-6c96b6">-</button>
 </template>
 
@@ -7,13 +8,13 @@
 import { useStore } from '../../store'
 const store = useStore()
 let reduction = setInterval(() => { });
-function continuousReduction() {
+function continuousReduction(event: string) {
     let isClick = true;
     reduction = setInterval(() => {
         isClick = false;
         store.levelReduction();
-    }, 240);//如果时间过小，会导致先执行下面的语句，从而造成点击会执行两次减少，暂时不清楚原因
-    isClick ? store.levelReduction() : "";
+    }, 240);//如果时间过小，会导致先执行计时器，即鼠标点击一次会导致多次更改，触摸点击无影响，可能跟事件处理顺序导致的延迟有关。
+    isClick && event == 'mousedown' ? store.levelReduction() : "";
 }
 function stopReduction() {
     clearInterval(reduction);
