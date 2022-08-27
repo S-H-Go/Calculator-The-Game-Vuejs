6fd94af0175cd86a3bf93610e59eca2b8f9c8c0b
S-H-G <wangshijiehh@outlook.com>
2022-08-27 09:53:24 +0800

    🐞 fix(setButton): 修复设置按钮没有按下的效果

--- src/components/setButton/SetButton.vue
+++ src/components/setButton/SetButton.vue
@@ -1,10 +1,16 @@
 <template>
-  <button @click="store.changeMode()" class="button color-button-yeelow">设置</button>
+  <button @click="setClick()" class="button color-button-yeelow">设置</button>
 </template>
 
 <script lang="ts" setup>
 import { useStore } from '../../store'
 const store = useStore()
+//添加延时使:active执行完之后再切换 ButtonPanel
+function setClick() {
+  setTimeout(() => {
+    store.changeMode()
+  }, 100);
+}
 </script>
 
 <style>
