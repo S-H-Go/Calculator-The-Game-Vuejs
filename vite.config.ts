1b25236ef471b6c887ba7b61af801d7195ca2b7e
shg <wangshijiehh@outlook.com>
2023-11-06 17:15:50 +0800

    🎈 perf: 更新名称，更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: "/calculator-game/",
+    base: "/Calculator-The-Game-Vuejs/",
     server: {
         host: true,
         port: 80,
